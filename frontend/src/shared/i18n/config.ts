a2225546ca7679ace349781d433b3a7d1adc937c
Kotovar <neasit3@gmail.com>
2025-01-22 15:41:40 +0700

    fix: update types for i18n

--- frontend/src/shared/i18n/config.ts
+++ frontend/src/shared/i18n/config.ts
@@ -1,16 +1,15 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import enNs from './en.json';
-import ruNs from './ru.json';
+import enTranslation from './data/en.json';
+import ruTranslation from './data/ru.json';
 
 i18n.use(initReactI18next).init({
-  fallbackLng: 'ru',
   defaultNS: 'translation',
-  resources: {
-    en: { translation: enNs },
-    ru: { translation: ruNs },
-  },
   lng: 'ru',
+  resources: {
+    ru: { translation: ruTranslation },
+    en: { translation: enTranslation },
+  },
   interpolation: {
     escapeValue: false,
   },
