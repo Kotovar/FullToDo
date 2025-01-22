f90c04ee2626c67875b8e0ffdd10ada3333816f9
Kotovar <neasit3@gmail.com>
2025-01-22 21:50:11 +0700

    feat: add settings for tests, add favicon

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig, loadEnv } from 'vite';
+import { loadEnv } from 'vite';
+import { coverageConfigDefaults, defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
@@ -17,6 +18,15 @@ export default defineConfig(({ mode }) => {
       css: true,
       passWithNoTests: true,
       mockReset: true,
+      coverage: {
+        provider: 'istanbul',
+        exclude: [
+          'src/main.tsx',
+          'src/shared/i18n/config.ts',
+          'src/shared/i18n/types/resources.ts',
+          ...coverageConfigDefaults.exclude,
+        ],
+      },
     },
     resolve: {
       alias: {
