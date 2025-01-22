3009e882c48ed242ee3aebc8fdb706a3fccaa1b3
Kotovar <neasit3@gmail.com>
2025-01-22 15:41:40 +0700

    fix: update types for i18n

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import App from '@/app/App';
 import '@/app/styles';
-import '@/shared/config/i18n';
+import '@/shared/i18n';
 
 const rootElement = document.getElementById('root')!;
 if (!rootElement.innerHTML) {
---
+++ frontend/src/shared/i18n/types/resources.ts
@@ -0,0 +1,7 @@
+import ruTranslation from '../data/ru.json';
+
+const resources = {
+  ruTranslation,
+} as const;
+
+export default resources;
