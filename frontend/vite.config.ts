190d0839ac2dbef4708514bf023fc5d4b346568c
Kotovar <neasit3@gmail.com>
2025-01-23 15:46:48 +0700

    feat: upgrade tailwind to 4.0

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { loadEnv } from 'vite';
 import { coverageConfigDefaults, defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig(({ mode }) => {
@@ -10,7 +11,7 @@ export default defineConfig(({ mode }) => {
     define: {
       'process.env': env,
     },
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     test: {
       globals: true,
       setupFiles: ['./setupTests.ts'],
