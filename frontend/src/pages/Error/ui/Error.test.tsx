9def86d711e2301e3759c5601c71cfa7ed859c2f
Kotovar <neasit3@gmail.com>
2025-04-15 14:38:34 +0700

    feat: finish Error test. Coverage 53 per cent

---
+++ frontend/src/pages/Error/ui/Error.test.tsx
@@ -0,0 +1,24 @@
+import { screen } from '@testing-library/react';
+import { describe, expect, test, vi } from 'vitest';
+import userEvent from '@testing-library/user-event';
+import { renderWithRouter } from '@shared/testing';
+import { Error } from './Error';
+
+describe('Error component', () => {
+  const user = userEvent.setup();
+  const onClickMock = vi.fn();
+
+  test('render correctly', async () => {
+    renderWithRouter(<Error />);
+
+    const heading = screen.getByRole('heading');
+    const button = screen.getByRole('button');
+    expect(heading).toBeDefined();
+    expect(button).toBeDefined();
+
+    button.onclick = onClickMock;
+
+    await user.click(button);
+    expect(onClickMock).toHaveBeenCalled();
+  });
+});
